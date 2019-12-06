82efb800cd354ff835c84157bb2d4bbdb2fd62ed
Anoque <anoque@mail.ru>
2019-12-06 16:55:53 +0700

    Add generate and retrieve methods

--- src/Controller/RandomController.php
+++ src/Controller/RandomController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Random;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -15,6 +16,47 @@ class RandomController extends AbstractController
         return $this->json([
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/RandomController.php',
+        ]);
+    }
+
+    /**
+     * @Route("/generate", name="generate")
+     */
+    public function showGenerate()
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $randomInt = random_int(-2147483648, 2147483647);
+
+        $random = new Random();
+        $random->setNumber($randomInt);
+
+        $entityManager->persist($random);
+        $entityManager->flush();
+
+        return $this->json([
+            'id' => $random->getId(),
+            'number' => $randomInt
+        ]);
+    }
+
+    /**
+     * @Route("/retrieve/{id}", name="retrieve")
+     */
+    public function showRetrieve(int $id) {
+        $random = $this->getDoctrine()
+            ->getRepository(Random::class)
+            ->find($id);
+
+        if (!$random) {
+            return $this->json([
+                'error' => 'No random number found by id ' . $id
+            ]);
+        }
+
+        return $this->json([
+            'id' => $id,
+            'number' => $random->getNumber()
         ]);
     }
 }
